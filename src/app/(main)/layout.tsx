ef4f021c8bd8199ac4c430437090c511b9edbe79
Michizane498 <141784707+Michizane498@users.noreply.github.com>
2025-04-24 21:01:19 +0300

    Settings

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "@/components/sidebar";
+import Infobar from "@/components/infobar";
 
 type Props = { children: React.ReactNode };
 
@@ -7,7 +8,10 @@ const Layout = (props: Props) => {
   return (
     <div className="flex overflow-hidden h-screen">
       <Sidebar />
-      <div className="w-full">{props.children}</div>
+      <div className="w-full">
+        <Infobar />
+        {props.children}
+      </div>
     </div>
   );
 };
